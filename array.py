7262bee18432f9feeaa591e1be182ef32d71de55
favoursui <samuelfavour858@gmail.com>
2025-09-15 12:43:32 +0100

    first commit

---
+++ array.py
@@ -0,0 +1,37 @@
+'''
+for num in range (1, 101):
+	if num % 2 == 0:
+		#print (num)
+		if num <= 60 or num >= 70:
+			print(num)
+'''
+from module import Array
+
+fruit=Array()
+fruit.append("food")
+fruit.append("banana")
+fruit.remove("banana")
+
+
+
+print(fruit.value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ module.py
@@ -0,0 +1,30 @@
+
+class Array:
+    def __init__(self):
+        self.value = []
+
+    def append(self, item):
+        self.value += [item] 
+
+    def pop(self, index =-1):
+        if index < 0:
+            index += len(self.value)
+        if index < 0 or index >= len(self.value):
+            raise IndexError("index out of range") 
+        else:   
+            item = self.value[index]
+            self.value = self.value[:index] + self.value[index +1:]
+            return item
+    
+    def insert(self, index, item):
+        if index < 0:
+            index = 0
+        if index > len(self.value):
+            index = len(self.value)
+        self.value = self.value[:index] + [item] + self.value[index:]          
+    
+    def remove(self, item):
+       for i in range(len(self.value)):
+           if self.value[i] == item:
+               self.value.pop(i)
+               return self.value
